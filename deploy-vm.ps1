e9a949750b0c4fdb7210dce2b5b500f84b21e884
bbsdimas <bbsdimas@gmail.com>
2021-10-07 13:11:28 +0500

    3

--- deploy-vm.ps1
+++ deploy-vm.ps1
@@ -1,5 +1,8 @@
 $verboseLogFile = "deployment.log"
 
+$param1=$args[0]
+$param2=$args[1]
+#write-host $param1
 
 
 
@@ -24,5 +27,10 @@ Function My-Logger {
 
 My-Logger "Deployment Complete!"
 My-Logger "StartTime: $StartTime"
+My-Logger "param1: $args[0]"
+My-Logger "param2: $args[1]"
+My-Logger "param3: $args[2]"
+My-Logger "param4: $args[3]"
+
 #My-Logger "EndTime: $EndTime"
 #My-Logger " Duration: $duration minutes"
