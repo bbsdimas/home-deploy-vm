9e2a6c6c784d994b549bec0fc8fd3a65326a1a36
bbsdimas <bbsdimas@gmail.com>
2021-10-07 12:51:07 +0500

    edited ps script

--- deploy-vm.ps1
+++ deploy-vm.ps1
@@ -1,1 +1,28 @@
-Import-vapp
+$verboseLogFile = "deployment.log"
+
+
+
+
+$StartTime = Get-Date
+
+Function My-Logger {
+    param(
+    [Parameter(Mandatory=$true)]
+    [String]$message
+    )
+
+    $timeStamp = Get-Date -Format "MM-dd-yyyy_hh:mm:ss"
+
+    Write-Host -NoNewline -ForegroundColor White "[$timestamp]"
+    Write-Host -ForegroundColor Green " $message"
+    $logMessage = "[$timeStamp] $message"
+    $logMessage | Out-File -Append -LiteralPath $verboseLogFile
+}
+
+
+
+
+My-Logger "Deployment Complete!"
+My-Logger "StartTime: $StartTime"
+#My-Logger "EndTime: $EndTime"
+#My-Logger " Duration: $duration minutes"
